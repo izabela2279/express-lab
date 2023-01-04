25733ba3630064f24efa3710e38a0c83fce9c228
Izabela <izabelaszeremeta@gmail.com>
2023-01-03 21:10:09 -0500

    initial commit

---
+++ data/workouts-data.js
@@ -0,0 +1,9 @@
+const workouts = [
+  {text: 'Pilates', done: true, _id: 100},
+  {text: 'Yoga', done: true, _id: 200},
+  {text: 'Running', done: false, _id: 300},
+]
+
+export { 
+	workouts
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+// import modules
+
+import express from 'express'
+import { workouts } from './data/workouts-data.js'
+
+console.log(workouts);
+
+
+// Create Express app
+
+const app = express()
+
+// Configure the app (app.set)
+app.set('view engine', 'ejs')
+
+
+// Mount Middleware (app.use)
+
+
+
+// Mount routes
+
+app.get('/', function(req, res) {
+  res.send('<h1>Start Express Lab</h1>')
+})
+
+app.get('/home', function(req, res) {
+  res.render('home')
+})
+
+app.get('/workouts', function(req, res) {
+  res.render('workouts/index', {
+    workouts: workouts
+  })
+})
+
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
